38ee3e3aeb899e1fb5677dd82a65b99c1eb707ad
Edwin Andermyr <andermyredwin@gmail.com>
2024-01-30 18:15:15 +0100

    updated nav but still issue in logic not happy yet :(

--- js/general.js
+++ js/general.js
@@ -9,17 +9,14 @@ $(() => {
 
     $(".main-menu").on("click", ".with-arrow[data-submenu]", function (event) {
       const $clickedWithArrow = $(this);
-      $clickedWithArrow.toggleClass("active");
       const $submenu = $clickedWithArrow.find(".submenu, .submenu-2, .submenu-3");
-      $submenu.slideToggle();
 
-      // Close other submenus
-      $(".with-arrow[data-submenu]").not($clickedWithArrow).removeClass("active").find(".submenu, .submenu-2, .submenu-3").slideUp();
+      // Remove active class from other submenus
+      $(".submenu, .submenu-2, .submenu-3").not($submenu).removeClass("active");
 
-      // If it's a submenu-2, ensure that the submenu-2 is visible
-      if ($clickedWithArrow.hasClass("submenu-2")) {
-        $clickedWithArrow.closest(".submenu-2").slideDown();
-      }
+      // Toggle the class to control visibility
+      $clickedWithArrow.toggleClass("active");
+      $submenu.toggleClass("active");
     });
   };
 
